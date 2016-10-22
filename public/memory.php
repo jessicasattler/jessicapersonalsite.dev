e6a05e5462fc397c797c1d0b0b85f3a72fd134a4
Jessica Sattler <jessicasattler28@gmail.com>
2016-10-22 17:11:01 -0500

    Tested changes in customStoryModifications (new file) and site will now be completely mobile responsive

---
+++ public/memory.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Regular Show Memory</title>
+</head>
+<body>
+<p>Regular</p>
+</body>
+</html>
---
+++ public/storyModifications.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>My Story</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/story.css" rel="stylesheet">
+    <!-- My own custom style sheet: -->
+    <link href="/css/customstory.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand"><img src="/img/logo.png"></h3>
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li><a href="/index.html"><span class="glyphicon glyphicon-home glyphicon-large" aria-hidden="true"></span></a></li>
+
+                  <li><a href="/resume2.php"><span class="glyphicon glyphicon-list-alt glyphicon-large" aria-hidden="true"></span></a></li>
+
+                  <li><a href="/portfolio.php"><span class="glyphicon glyphicon-folder-close glyphicon-large" aria-hidden="true"></span></a></li>
+
+                  <li class="active"><a href="/story.php"><span class="glyphicon glyphicon-book glyphicon-large" aria-hidden="true"></span></a></li>
+
+                  <li><a href="/contact.php"><span class="glyphicon glyphicon-envelope glyphicon-large" aria-hidden="true"></span></a></li>
+
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+               <h2  id="careerStory">My Career Story</h2>
+               <div id="careerText">
+                  
+                  <p>As an immigrant from Mexico, I grew up with the constant reminder that my parents sacrificed their financial stability in order for my sisters and I to have more educational and vocational opportunities. Because of this, I felt I had to pursue a career that contributed to both society and my family. From early on, I thought this career would be in engineering, where I could solve complex problems directly affecting people’s lives.
+                  </p>
+                  <p>However, when it came time to choose a major in college, I couldn’t quite make the leap. I had taken a few math classes, but felt as though I did not fit the stereotype of what a “computer science engineer” was. I became afraid to fail and disappoint my family, so I choose fields of study I thought I could safely succeed in: Psychology and Business.  
+                  </p>
+                  
+               </div>
+
+                   <button type="button" id="leftButton" class="btn btn-default btn-lg">
+                     <span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> 
+                  </button>
+                  <button id="pageNum">1</button>
+                   <button type="button" class="btn btn-default btn-lg" id="rightButton">
+                     <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span> 
+                  </button>
+                
+                <div class="tableParent">
+                  <h2 id="lifeOutside">Life Outside of Software Development</h2>
+                    <div class = "row">
+                      <div class = "col-sm-6 col-xs-12 zipliningClass" id="addWords" >
+                       <img class="picture" id="ziplining" src="/img/zipliningDream.jpg">
+                      </div>
+                      <div class = "col-sm-6 col-xs-12 whitAndJessClass" id="addWords2">
+                        <img class="picture" id="whitAndJess" src="/img/whitAndJessDream2.jpg">
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-sm-6 col-xs-12 fabiClass" id="addWords3">
+                        <img class="picture3" id="fabi" src="/img/FabiDream.jpg">
+                      </div>
+                      <div class="col-sm-6 col-xs-12 chinaClass" id="addWords4" >
+                        <img class="picture" id="china" src="/img/chinaDream2.jpg">
+                      </div>
+                    </div>
+ 
+                </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
+    <script src="/js/jQuery.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/story.js"></script>
+   
+  </body>
+</html>
